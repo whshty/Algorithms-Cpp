7ab8376e25e18f386bddd81f802bc08966888db2
Teng Yang <yangteng0813@gmail.com>
2022-11-28 23:26:17 -0500

    11.28

---
+++ Solution/3. Longest Substring Without Repeating Characters/Solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.length() == 0) return 0;
+        unordered_map<char, int> map;
+        int i = 0, j = 0, res = INT_MIN;
+        while(j<s.length()) {
+            map[s[j]]++;
+            if(map.size() == j-i+1) {
+                res = max(res,j-i+1);
+            }
+            else if(map.size() < j - i + 1) {
+                while(map.size() < j- i + 1) {
+                    map[s[i]]--;
+                    if(map[s[i]] == 0) {
+                        map.erase(s[i]);
+                    }
+                    i++;
+                }     
+            }
+            j++;
+        }
+        return res; 
+    }
+};
