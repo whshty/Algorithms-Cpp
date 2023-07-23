e2b1e8305b6706c6eef9ac7b28acd2fb4535f2af
Teng Yang <yangteng0813@gmail.com>
2023-07-23 12:48:19 -0400

    7.23

--- Solution/3. Longest Substring Without Repeating Characters/Solution.cpp
+++ Solution/3. Longest Substring Without Repeating Characters/Solution.cpp
@@ -4,13 +4,13 @@ public:
         if (s.length() == 0) return 0;
         unordered_map<char, int> map;
         int i = 0, j = 0, res = INT_MIN;
-        while(j<s.length()) {
+        while (j <s.length()) {
             map[s[j]]++;
-            if(map.size() == j-i+1) {
+            if (map.size() == j-i+1) {
                 res = max(res,j-i+1);
             }
-            else if(map.size() < j - i + 1) {
-                while(map.size() < j- i + 1) {
+            else if (map.size() < j - i + 1) {
+                while (map.size() < j- i + 1) {
                     map[s[i]]--;
                     if(map[s[i]] == 0) {
                         map.erase(s[i]);
